45d1ae21cacd9e70d7b254e64db8a2e07b29f7fa
Lucas <lucasgrafimar@gmail.com>
2022-08-06 13:41:50 -0300

    .

--- p4.pl
+++ p4.pl
@@ -1,15 +1,17 @@
 :- include(input).
 
+exibeCaminho(X,Y,Len) :-
+  caminho(X,Y,Len),
+  write(X),
+  write(".").
 
+caminho(X,Y,Len) :-
+  pode_ir(X,Y,Len,_),
+  write(Y),
+  write(", ").
 
-caminho(Inicio,Destino,Len) :-
-  pode_ir(Inicio,Destino,Len,Linha),
-  write("Pasou pela linha: "),write(Linha),nl,
-  write("Pasou pela estação: "),write(Destino),nl.
-
-caminho(Inicio,Destino,Len) :-
-  pode_ir(Inicio,Intermediario,L,Linha),
-  caminho(Intermediario,Destino,L1),
-  Len is L + L1,
-  write("Pasou pela linha: "),write(Linha),nl,
-  write("Pasou pela estação: "),write(Intermediario),nl.
+caminho(X,Y,Len) :-
+  pode_ir(X,Z,L,_),
+  caminho(Z,Y,L1),
+  write(Z), write(", "),
+  Len is L + L1.
